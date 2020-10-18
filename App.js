88c50d673084440fa3b3a01179eff208ef775412
Andres <andresg5997@gmail.com>
2020-10-18 18:07:13 +0200

    working app & build script done

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { WebView } from 'react-native-webview';
+import axios from 'axios';
+import { Text } from 'react-native';
+import { apiUrl, salonId, salonUrl } from './imports';
 
 export default function App() {
-  const uri = 'https://beban-barber-shop-2.de/';
+  const [selectedSalonUrl, setSelectedSalonUrl] = useState(salonUrl);
 
-  return (
-    <>
-      <WebView source={{ uri }} />
-    </>
-  );
+  useEffect(() => {
+    // fetchSalonData();
+    setSelectedSalonUrl(salonUrl);
+  }, []);
+
+  // const fetchSalonData = async () => {
+  //   const url = `${apiUrl}GetSalonByGuid?guid=${salonId}`;
+
+  //   try {
+  //     const res = await axios.get(url);
+  //     setSelectedSalonUrl(res.data.Url);
+  //     return res.data;
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  //   return {};
+  // };
+
+  return <WebView source={{ selectedSalonUrl }} />;
 }
---
+++ imports.js
@@ -0,0 +1,5 @@
+const salonUrl = 'https://beban-barber-shop-2.de';
+const salonId = 'e37d8a34-340b-4048-bd07-c9f793c1dc8a';
+const apiUrl = 'http://demosalon.hilgensoft.de/Api/apis/';
+
+export { salonUrl, salonId, apiUrl };
