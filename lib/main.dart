59e23dff7bda8dfe9ffdf3de239cd8eb7f91b046
Andres <andresg5997@gmail.com>
2020-10-28 00:40:05 +0100

    added flutter app

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl: 'https://beban-barber-shop-2.de',
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
