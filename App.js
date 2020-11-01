1d63743c0615380e1d77e90ca74359dc35a7ae37
Andres <andresg5997@gmail.com>
2020-11-01 20:33:41 +0100

    added safe area view

--- App.js
+++ App.js
@@ -1,18 +1,25 @@
 import { WebView } from 'react-native-webview';
 import React from 'react';
-import { ActivityIndicator, Text } from 'react-native';
+import { ActivityIndicator, StyleSheet, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <Text>Test</Text>
-    // <WebView
-    //   source={{ uri: 'https://beban-barber-shop-2.de' }}
-    //   startInLoadingState
-    //   renderLoading={() => <ActivityIndicator />}
-    //   onError={(syntheticEvent) => {
-    //     const { nativeEvent } = syntheticEvent;
-    //     console.warn('WebView error: ', nativeEvent);
-    //   }}
-    // />
+    <SafeAreaView style={styles.container}>
+      <WebView
+        source={{ uri: 'https://beban-barber-shop-2.de' }}
+        startInLoadingState
+        renderLoading={() => <ActivityIndicator />}
+        onError={(syntheticEvent) => {
+          const { nativeEvent } = syntheticEvent;
+          console.warn('WebView error: ', nativeEvent);
+        }}
+      />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+})
