ac195869dea4870178733fc7acfe17ca05f300c9
Andres <andresg5997@gmail.com>
2021-04-16 23:54:16 +0200

    added push notifications and new app images
    remaining correct messages

--- App.js
+++ App.js
@@ -1,18 +1,77 @@
 import { WebView } from 'react-native-webview';
 import React from 'react';
-import { ActivityIndicator, StyleSheet, SafeAreaView } from 'react-native';
+import { ActivityIndicator, StyleSheet, SafeAreaView, Text } from 'react-native';
+import PushNotification, { PushNotificationObject } from 'react-native-push-notification';
+const injectedJS = `
+window.addEventListener('sendData', function() {
+  const res = localStorage.getItem('PUSH_NOTIFICATION')
+  if(window.ReactNativeWebView)
+    window.ReactNativeWebView.postMessage(res);
+});true;`
 
 export default function App() {
+  
+  const runNotificationTimer = (e) => {
+
+    setTimeout(() => {
+      PushNotification.getScheduledLocalNotifications((data) => {
+        console.log(JSON.stringify(data))
+      });
+    }, 2500);
+    
+    const { nativeEvent } = e;
+    console.log("🚀 ~ file: App.js ~ line 23 ~ runNotificationTimer ~ nativeEvent", nativeEvent)
+
+    const form = JSON.parse(nativeEvent.data);
+
+    const {
+      remainingMinutes,
+      form: {
+        date,
+        time,
+        stylistName
+      }
+    } = form;
+
+    console.log("🚀 ~ file: App.js ~ line 31 ~ runNotificationTimer ~ form", form)
+
+    PushNotification.localNotificationSchedule({
+      channelId: "your-great-notification",
+      message: `Sie gerade haben ein Termin gebucht mit ${stylistName} um ${time} am ${date}`,
+      date: new Date(Date.now() + 1 * 1000), 
+    })
+
+    // Two hours reminder
+    if(remainingMinutes > (60 * 2)) {
+      PushNotification.localNotificationSchedule({
+        channelId: "your-great-notification",
+        message: `Sie haben ein Termin mit ${stylistName} um ${time} am ${date}`,
+        date: new Date(Date.now() + ((remainingMinutes - (60 * 2)) * 60) * 1000), 
+      })
+    }
+
+    // 24 hours reminder
+    if(remainingMinutes > (60 * 24)) {
+      PushNotification.localNotificationSchedule({
+        channelId: "your-great-notification",
+        message: `Sie haben ein Termin mit ${stylistName} um ${time} am ${date}`,
+        date: new Date(Date.now() + ((remainingMinutes - (60 * 24)) * 60) * 1000), 
+      })
+    }
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <WebView
-        source={{ uri: 'https://beban-barber-shop-2.de' }}
+        injectedJavaScript={ injectedJS }
+        source={{ uri: 'http://10.0.2.2:9000/' }}
         startInLoadingState
         renderLoading={() => <ActivityIndicator />}
         onError={(syntheticEvent) => {
           const { nativeEvent } = syntheticEvent;
           console.warn('WebView error: ', nativeEvent);
         }}
+        onMessage={runNotificationTimer}
       />
     </SafeAreaView>
   );
--- index.js
+++ index.js
@@ -2,6 +2,20 @@ import { registerRootComponent } from 'expo';
 
 import App from './App';
 
+import PushNotification from 'react-native-push-notification';
+
+PushNotification.createChannel(
+  {
+    channelId: "your-great-notification", // (required)
+    channelName: "My channel", // (required)
+    channelDescription: "A channel to categorise your notifications", // (optional) default: undefined.
+    soundName: "default", // (optional) See `soundName` parameter of `localNotification` function
+    importance: 4, // (optional) default: 4. Int value of the Android notification importance
+    vibrate: true, // (optional) default: true. Creates the default vibration patten if true.
+  },
+  (created) => console.log(`createChannel returned '${created}'`) // (optional) callback returns whether the channel was created, false means it already existed.
+);
+
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
